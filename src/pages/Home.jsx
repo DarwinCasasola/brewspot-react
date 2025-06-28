7661e2e82975f59f02ca9132d38f81c8e67fab6f
DarwinCasasola14 <you@example.com>
2025-06-27 22:46:37 -0700

    3.2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ function Home() {
   return (
     <div className="text-center">
       <h1 className="mb-3">Welcome to BrewSpot</h1>
-      <p>Discover the best coffee Las Vegas has to offer.</p>
+      <p><h4>Discover the best local coffee Las Vegas has to offer.</h4></p>
       <img
         src="/public/images/generatedcoffeevibes.jpg"
         alt="Featured Coffee"
         className="img-fluid rounded my-4"
-        style={{ maxWidth: "400px" }}
+        style={{ maxWidth: "600px" }}
       />
       <br />
       <Link to="/shops" className="btn btn-primary">Explore Coffee Shops</Link>
