ad5ae98e13b2f5e4270d9470fa4f31e0c8499744
DarwinCasasola14 <you@example.com>
2025-06-27 22:57:38 -0700

    last index image try

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import coffeeimg from "../assets/generatedcoffeevibes.jpg";
 
 function Home() {
   return (
@@ -6,8 +7,8 @@ function Home() {
       <h1 className="mb-3">Welcome to BrewSpot</h1>
       <p><h4>Discover the best local coffee Las Vegas has to offer.</h4></p>
       <img
-        src="public/images/generatedcoffeevibes.jpg"
-        alt="Featured Coffee"
+        src={coffeeimg}
+        alt="BrewSpot Coffee Vibes"
         className="img-fluid rounded my-4"
         style={{ maxWidth: "600px" }}
       />
