a29bcdffc56b6472d40a426ff2ec62600a746d2b
DarwinCasasola14 <you@example.com>
2025-06-28 07:25:21 -0700

    mobilescreen fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ import coffeeimg from "../assets/generatedcoffeevibes.jpg";
 
 function Home() {
   return (
-    <div className="text-center">
+    <div className="container text-center">
       <h1 className="mb-3">Welcome to BrewSpot</h1>
       <p><h4>Discover the best local coffee Las Vegas has to offer.</h4></p>
       <img
         src={coffeeimg}
         alt="BrewSpot Coffee Vibes"
-        className="img-fluid rounded my-4"
-        style={{ maxWidth: "600px" }}
+        className="img-fluid rounded mx-auto d-block"
+        style={{ maxWidth: "100%", height: "auto" }}
       />
       <br />
       <Link to="/shops" className="btn btn-primary">Explore Coffee Shops</Link>
