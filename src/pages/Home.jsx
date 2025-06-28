98e1c1ac7ae5ea3a08ecb34d4769fa336d2c5995
DarwinCasasola14 <you@example.com>
2025-06-27 22:51:14 -0700

    index image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <h1 className="mb-3">Welcome to BrewSpot</h1>
       <p><h4>Discover the best local coffee Las Vegas has to offer.</h4></p>
       <img
-        src="/public/images/generatedcoffeevibes.jpg"
+        src="images/generatedcoffeevibes.jpg"
         alt="Featured Coffee"
         className="img-fluid rounded my-4"
         style={{ maxWidth: "600px" }}
